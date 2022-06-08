1519f9ad057c9574d9654a6c3babdce53faa9863
AxelTur-Springer <axel.turspringer@gmail.com>
2022-06-08 11:10:36 -0300

    fixuing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
 import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword , 
+  onAuthStateChanged, 
+  signOut,
+  signInWithEmailAndPassword} from "firebase/auth"
+import { auth } from "./firebase/firebaseconfig";
+import {loginSuccess, logoutSuccess,UserLoginName} from "./features/loginCheck/loginSlice"
+
 import React from 'react';
 import { useState,useEffect } from 'react';
 import { BrowserRouter, Route, Routes,Link,Navigate } from 'react-router-dom';import './App.css';
 import Login from "./pages/Login";
 import NavBar from "./Components/NavBar";
 import HomeScreen from "./pages/homeScreen";
+import Main from "./pages/main";
 import { store } from "./app/store";
 import { useSelector, useDispatch } from 'react-redux';
 function App() {
   const [user,setUser] = useState()
-  const Store = useSelector((store)=>{return store})
-  useEffect(() => {
-    setUser(Store.LogedInReducer.userLogIn);
-  },);
- 
+  const dispatch = useDispatch();
 
-  
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser.email);
+           if(currentUser!== null){
+             dispatch(loginSuccess("true"))
+              //dispatch(UserLoginName(user.email))
+          }
+  });
+  },);
+
   return (
     <div className="App">
       <BrowserRouter > 
-      {user}   
       <NavBar />
+      {user }
       <Routes>
-        <Route path='/'  element = {  user ==="true"? (
-              <Navigate replace to="/home" />
+        <Route path='/'  element = {  user !== undefined? (
+              <Main />
             ) : (
-              <Login />
+              <HomeScreen/>
             )}/>
         <Route path='/home' element = {<HomeScreen/>}/>
         <Route path='/login' element = {<Login />} />
-      </Routes>
+        <Route path='/main' element = {<Main/>} />
+</Routes>
     </BrowserRouter >
     </div>
   );
---
+++ src/pages/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            Main Content
+        </div>
+    );
+}
+
+export default Main;
