9d15754d79ab4f42a3f33b261eccd77462af87c2
AxelTur-Springer <axel.turspringer@gmail.com>
2022-06-10 16:13:41 -0300

    changes

--- src/MovieApiData.js
+++ src/MovieApiData.js
@@ -21,5 +21,5 @@ async function popularApi() {
     }
   }
 
-  
+
 export {popularApi,originalSeriesapi}
--- src/pages/main.js
+++ src/pages/main.js
@@ -3,28 +3,35 @@ import { useState,useEffect } from 'react';
 import NavBar from '../Components/NavBar';
 import "../styling/main.css"
 //import { getMovieList ,apiimg } from '../MovieApiData';
-import { originalSeriesapi,popularApi } from '../MovieApiData';
+import { originalSeriesapi,popularApi ,allMovies} from '../MovieApiData';
 
 const Main = () => {
-    const [AllmoviesSeries,setAllmoviesSeries] = useState([])
     const [originalSeries,setoriginalSeries] = useState([])
     const [popular,setpopular] = useState([])
+
+
     const API_KEY = "076a8aeadc245f1cbc3779685ceb3a1d";
     useEffect(() => {
-     originalSeriesapi().then((data)=>{setoriginalSeries(data.results) })
-     popularApi().then((data)=>{setpopular(data.results) })
-     setAllmoviesSeries([...originalSeries,...popular])
-     console.log(AllmoviesSeries)
-    }, []);
-    
-    let x = Math.floor((Math.random() * 40) + 1);
+        originalSeriesapi().then((data)=>{setoriginalSeries(data.results) })
+        popularApi().then((data)=>{setpopular(data.results) })
+        console.log(originalSeries)
+    },[]);
+
+
     return (
         <>
             <NavBar />
             <div className='Main-menu'>
                 <div className='preview-menu'>
-                <PreviewMenu img ={    AllmoviesSeries[x] === undefined ? null : "https://image.tmdb.org/t/p/w500" +AllmoviesSeries[x].backdrop_path }/>
-            
+                { originalSeries.filter((movie)=>{
+                            return movie.name === "Stranger Things"
+                }
+                ).map((movie)=>{
+                    return <PreviewMenu 
+                    img={"https://image.tmdb.org/t/p/w1280" + movie.backdrop_path} />
+                    })
+            }
+                            
                 </div>
                 <div className='categories'>
                     <div className='carusel OriginalSeries'>
@@ -52,7 +59,7 @@ const Main = () => {
 
 function PreviewMenu(props){
     return(
-        <div>
+        <div className='previewMenu'>
             <img src={props.img} alt="" />
         </div>
     )
