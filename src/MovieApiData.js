57782b1efbd167582a294c21c97bc3b2ca5d1980
AxelTur-Springer <axel.turspringer@gmail.com>
2022-06-10 14:19:09 -0300

    changes

--- src/MovieApiData.js
+++ src/MovieApiData.js
@@ -2,16 +2,13 @@ const API_KEY = "076a8aeadc245f1cbc3779685ceb3a1d";
 
 
 
-/*export async function getMovieList() {
+export async function originalSeriesapi() {
     try{
       let response = await fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`)
       let data = await response.json();
-      console.log(data)     
+      return  await data
     }catch(err){
       console.error(err);
     }
   }
-  export function apiimg(){
-    return `https://image.tmdb.org/t/p/w500/`
-  }
-  console.log(getMovieList())*/
+
