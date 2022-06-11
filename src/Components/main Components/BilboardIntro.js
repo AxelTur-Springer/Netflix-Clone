b60d57a001d96d1f3aaf8ea505230771586dac1e
AxelTur-Springer <axel.turspringer@gmail.com>
2022-06-11 09:49:16 -0300

    changes

---
+++ src/Components/main Components/BilboardIntro.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useState,useEffect } from 'react';
+import randomNetflixMovies from '../../randomMovieObj';
+export const BilboardMain = () => {
+
+    const [randomOriginalNet,setrandomOriginalNet] = useState([])
+
+    
+    useEffect(() => {
+   
+        testing()
+    },[]);
+
+
+
+
+function testing(){
+    let arrayOfPreviewsCompo =[];
+    let x = 0;
+
+    for(let i = 0 ; i< randomNetflixMovies().length ; i++){
+        arrayOfPreviewsCompo.push(
+            <PreviewMenu 
+            img ={"https://image.tmdb.org/t/p/w1280" + 
+            randomNetflixMovies()[i].backdrop_path} />
+        )
+    }
+    setrandomOriginalNet(arrayOfPreviewsCompo[x])
+    setInterval(() => {
+        x === 20 ? x = 0 : x+=1
+        console.log(x)
+        setrandomOriginalNet(arrayOfPreviewsCompo[x])
+    }, 5000);
+}
+
+    return (
+                <>
+                    {randomOriginalNet}          
+                </>
+    );
+}
+
+
+
+function PreviewMenu(props){
+    return(
+        <div className='previewMenu'>
+            <img src={props.img} alt="" />
+        </div>
+    )
+}
--- src/pages/main.js
+++ src/pages/main.js
@@ -3,50 +3,28 @@ import { useState,useEffect } from 'react';
 import NavBar from '../Components/NavBar';
 import "../styling/main.css"
 import { originalSeriesapi,popularApi ,allMovies} from '../MovieApiData';
-import randomNetflixMovies from '../randomMovieObj';
+import { BilboardMain } from '../Components/main Components/BilboardIntro';
 const Main = () => {
     const [originalSeries,setoriginalSeries] = useState([])
     const [popular,setpopular] = useState([])
-    const [randomOriginalNet,setrandomOriginalNet] = useState([])
 
     const API_KEY = "076a8aeadc245f1cbc3779685ceb3a1d";
     
     useEffect(() => {
         originalSeriesapi().then((data)=>{setoriginalSeries(data.results) })
         popularApi().then((data)=>{setpopular(data.results) })
-        testing()
     },[]);
 
 
 
 
-function testing(){
-    let arrayOfPreviewsCompo =[];
-    let x = 0;
 
-    for(let i = 0 ; i< randomNetflixMovies().length ; i++){
-        arrayOfPreviewsCompo.push(
-            <PreviewMenu 
-            img ={"https://image.tmdb.org/t/p/w1280" + 
-            randomNetflixMovies()[i].backdrop_path} />
-        )
-    }
-    setrandomOriginalNet(arrayOfPreviewsCompo[x])
-    setInterval(() => {
-        x === 20 ? x = 0 : x+=1
-        console.log(x)
-        setrandomOriginalNet(arrayOfPreviewsCompo[x])
-    }, 5000);
-}
 
     return (
         <>
       <NavBar />
-
             <div className='Main-menu'>
-                <div className='preview-menu'>
-                    {randomOriginalNet}          
-                </div>
+                 <BilboardMain/>        
                 <div className='categories'>
                     <div className='carusel OriginalSeries'>
                             {
