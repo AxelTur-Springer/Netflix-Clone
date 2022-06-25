69cba307122ccd419dba998bcebd9b0480a5919b
AxelTur-Springer <axel.turspringer@gmail.com>
2022-06-25 16:13:34 -0300

    testing for bug in vercel

--- src/firebase/firebaseconfig.js
+++ src/firebase/firebaseconfig.js
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
-const API_KEY = process.env.REACT_APP_API_KEYS.split(",")[1]
+const API_KEY = process.env.REACT_APP_API_KEYS//.split(",")[1]
 const firebaseConfig = {
-    apiKey: API_KEY, //use enviroment variable
+    apiKey: API_KEY,
     authDomain: "netflixclone-d8d7d.firebaseapp.com",
     projectId: "netflixclone-d8d7d",
     storageBucket: "netflixclone-d8d7d.appspot.com",
